5ce8b9c4d1c8b1535743cbdd45d8d18b83001719
superligalv <superligalv@gmail.com>
2026-02-06 12:24:58 +0100

    add admin tools

---
+++ admin-svl/admin-teams.js
@@ -0,0 +1,51 @@
+const container = document.getElementById("teamsContainer");
+
+fetch("../JS/teams.json")
+.then(res => res.ok ? res.json() : Promise.reject("Error cargando teams.json"))
+.then(teams => {
+if (!teams.length) {
+container.innerHTML = "No hay equipos.";
+return;
+}
+
+```
+let html = `
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Equipo</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+`;
+
+teams.forEach(team => {
+  html += `
+    <tr>
+	  <td><img src="../images/flags/headerRund/${team.id}.png" alt="${team.id}" width="30" height="30"></td>
+      <td>${team.id}</td>
+      <td>${team.team}</td>
+      <td>
+        <a href="../ver_equipo.html?id=${team.id}" target="_blank">
+          Ver
+        </a>
+      </td>
+    </tr>
+  `;
+});
+
+html += `
+    </tbody>
+  </table>
+`;
+
+container.innerHTML = html;
+```
+
+})
+.catch(err => {
+container.innerHTML = "Error cargando equipos";
+console.error(err);
+});
