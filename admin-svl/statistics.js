99337ca591952dd67c182b76d6523736725c1367
superligalv <superligalv@gmail.com>
2026-05-11 12:24:39 +0200

    add statistics

--- admin-svl/statistics.js
+++ admin-svl/statistics.js
@@ -265,7 +265,7 @@ function renderRanking(
 // ======================================
 // MAIN
 // ======================================
-fetch("./teams.json")
+fetch("../JS/teams.json")
 
   .then(res => {
 
